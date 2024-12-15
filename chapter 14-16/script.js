9654db17d443f362b0abf832b6c7904d94181d56
Muhammad Sarfraz <msowner.best@gmail.com>
2024-12-15 21:54:09 +0500

    ch 14-16

---
+++ chapter 14-16/script.js
@@ -0,0 +1,38 @@
+
+//////////////////// chapter 12-13 ::::::: Assignment Number 1 ////////////////////
+
+var array = [];
+//////////////////// chapter 12-13 ::::::: Assignment Number 3 ////////////////////
+
+var string_array = ["Faizan", "Ahmed", "Mohsin", "Sarfraz"];
+console.log(string_array)
+
+
+//////////////////// chapter 12-13 ::::::: Assignment Number 4 ////////////////////
+
+var number_array = [10, 20, 30, 40, 50];
+console.log(number_array)
+
+//////////////////// chapter 12-13 ::::::: Assignment Number 5 ////////////////////
+
+var boolean_array = [true, false, true, false];
+console.log(boolean_array)
+
+
+//////////////////// chapter 12-13 ::::::: Assignment Number 6 ////////////////////
+
+var mixed_array = [true, false, "Sarfraz", "Ahmed", null, 12, 25, 26];
+console.log(mixed_array)
+
+
+//////////////////// chapter 12-13 ::::::: Assignment Number 7 ////////////////////
+
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+console.log("1 ...." + qualifications[0])
+console.log("2 ...." + qualifications[1])
+console.log("3 ...." + qualifications[2])
+console.log("4 ...." + qualifications[3])
+console.log("5 ...." + qualifications[4])
+console.log("6 ...." + qualifications[5])
+console.log("7 ...." + qualifications[6])
+console.log("8 ...." + qualifications[7])
