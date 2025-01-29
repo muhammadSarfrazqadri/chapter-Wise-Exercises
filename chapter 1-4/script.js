70987137da6b40d0bde5fb01a4eb44838c6b2a38
Muhammad Sarfraz <msowner.best@gmail.com>
2025-01-29 12:00:49 +0500

    chapter 1-4 complete

---
+++ chapter 1-4/script.js
@@ -0,0 +1,41 @@
+//////////////////// chapter 1 ::::::: Assignments ////////////////////
+
+alert('Welcome to Our Website')
+alert('Error! Please Enter a Valid Password.')
+alert('Welcome to Js Code ! \n Happy Coding.')
+
+//////////////////// chapter 2 ::::::: Assignments ////////////////////
+
+var userName = 'Muhammad Sarfraz'
+var age = '22'
+var email = 'muhammadsarfraz9726130@gmail.com'
+
+//////////////////// chapter 3 ::::::: Assignments ////////////////////
+
+var userAge = prompt('Enter Your Age !')
+var birthYear = ("Enter Your Birth's Year !")
+alert(`Your Age is ${userAge}`)
+alert(`Your Birth's Year is ${birthYear}`)
+
+// 3 variables in one statement
+
+var user1 = "Raza", user3 = "Zia-ud-deen", user2 = "Ilyaas";
+
+// 5 legal variable names
+
+var myName, _age, $price, firstName, my_variable;
+
+// Declare 5 illegal variable names (commented out to avoid errors)
+// var 1number;
+// var @symbol;
+// var my-name;
+// var break;
+// var alert;
+
+alert(
+    "Rules for Naming JavaScript Variables:\n\n" +
+    "Variable names can only contain letters, numbers, $ and _. For example: $my_1variable.\n" +
+    "Variable must begin with a letter, $ or _. For example: $name, _name or name.\n" +
+    "Variable names are case sensitive.\n" +
+    "Variable names should not be JavaScript keywords."
+);
